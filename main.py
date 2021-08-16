a7957bb02e551d99ab13ab3d39c4c6c286c8876b
Anell123 <anellchuquitucto14@gmail.com>
2021-08-16 17:25:24 -0500

    Se agregan cambios en el archivo main.py con un print de la carrera y también la confirmación.

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ Dni = 75744004
 Edad = 21
 Curso = "Github"
 Empresa= "Tistmart"
+Carrera="Ing.de sistemas"
 
-print(f"Hola me llamo {Nombre} {Apellido} mi Dni es {Dni} tengo {Edad} años , estoy haciendo el curso {Curso} que me ofrecio la empresa {Empresa}")
+print(f"Hola me llamo {Nombre} {Apellido} mi Dni es {Dni} tengo {Edad} años , estoy haciendo el curso {Curso} que me ofrecio la empresa {Empresa} de la carrera de {Carrera}")
