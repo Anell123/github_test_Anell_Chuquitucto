9264f81c7d895ba6667b5b2033fcd4aeeabc8f33
Anell123 <anellchuquitucto14@gmail.com>
2021-08-16 17:20:06 -0500

    Se crea un programa simple en python con menos de lineas con el nombre main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+Nombre = "Anell"
+Apellido ="Chuquitucto"
+Dni = 75744004
+Edad = 21
+Curso = "Github"
+Empresa= "Tistmart"
+
+print(f"Hola me llamo {Nombre} {Apellido} mi Dni es {Dni} tengo {Edad} años , estoy haciendo el curso {Curso} que me ofrecio la empresa {Empresa}")
